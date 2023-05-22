cf5995a1e7e0a3d7f084ca51f95258d452ef8398
biff cross <biffcross@hotmail.co.uk>
2023-05-22 01:16:25 +0100

    url update

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -12,7 +12,7 @@ const config: GatsbyConfig = {
     siteTitle: `Biff Cross`,
     siteTitleAlt: `Biff Cross Photography`,
     siteHeadline: `Photography by Biff Cross`,
-    siteUrl: `https://biffcross.github.io/portfolio`,
+    siteUrl: `https://biffcross.github.io/portfolio-website`,
     siteDescription: `Image-heavy photography portfolio with colorful accents & customizable pages. Includes adaptive image grids powered by CSS grid and automatic image integration into projects.`,
     siteImage: `/banner.jpg`,
     siteLanguage: `en`,
